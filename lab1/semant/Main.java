56d0a34cbba06246fb569227d40a5f7c630faeee
= <fellan12@gmail.com>
2017-04-09 16:09:14 +0200

    Created a VM class and added useful functions to Configurations class

--- lab1/semant/Main.java
+++ lab1/semant/Main.java
@@ -4,16 +4,35 @@ import semant.whilesyntax.Stm;
 import semant.amsyntax.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Stm s = WhileParser.parse(args[0]);
-        Code y = s.accept(new CompileVisitor());
-        for (Inst i : y) {
-        	System.out.println(i);
-        }
+	private static void pressKeyToContinue(){ 
+		System.out.println("Press any key to continue...");
+		try{
+			System.in.read();
+		}  
+		catch(Exception e){
+		}  
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// - Compile s into AM Code
+		Stm s = WhileParser.parse(args[0]);
+		Code y = s.accept(new CompileVisitor());
 
-        
-        // TODO:
-        // - Compile s into AM Code
+		/*
+		for (Inst i : y) {
+			System.out.println(i);
+		}
+		*/
+		
+		Configuration conf = new Configuration(y);
+		VirtualMachine VM = new VirtualMachine();
+		while(conf.hasNext()){
+			conf.printConfig();
+			conf = VM.step(conf);
+			pressKeyToContinue();
+		}
+		conf.printConfig();
+
         // - Execute resulting AM Code using a step-function.
-    }
+	}
 }
